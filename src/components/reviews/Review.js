92608fcead2e0cd454007a6fcef1f865d872e0fc
sadatakhtar <sadat0akhtar@gmail.com>
2021-02-01 20:28:08 +0000

    media queries for 1024 & 768 pixals

--- src/components/reviews/Review.js
+++ src/components/reviews/Review.js
@@ -9,18 +9,18 @@ function Review() {
     return (
         <div className="review_container">
             <div className="review_customer1">
-                <img src={customer1} width="250px" height="250px" alt="customer"/>
-                <img src={speachmarks} width="70px" alt="speech marks"/>
+                <img id="cus1" src={customer1} width="250px" height="250px" alt="customer"/>
+                <img id="speech1" src={speachmarks} width="70px" alt="speech marks"/>
                 <p>Thank you so much for coming out so quickly. Brilliant service!</p>
             </div>
             <div className="review_customer2">
-                <img src={customer2} width="250px" height="250px" alt="customer"/>
-                <img src={speachmarks} width="70px" alt="speech marks"/>
+                <img id="cus2" src={customer2} width="250px" height="250px" alt="customer"/>
+                <img id="speech2" src={speachmarks} width="70px" alt="speech marks"/>
                 <p>Can't believe how good the service was. Absolutely amazing!!!</p>
             </div>
             <div className="review_customer3">
-                <img src={customer3} width="250px" height="250px" alt="customer"/>
-                <img src={speachmarks} width="70px" alt="speech marks"/>
+                <img id="cus3" src={customer3} width="250px" height="250px" alt="customer"/>
+                <img id="speech3" src={speachmarks} width="70px" alt="speech marks"/>
                 <p>The only highlight of the day was meeting the recovery man and the treatment we recieved.</p>
             </div>
             
